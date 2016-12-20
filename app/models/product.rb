7c0a85f59005dec59a227199f11073a777014dd0
A-Boudi <abr.boudi@gmail.com>
2016-12-20 18:17:49 +0100

    fix relation between product and photos in the prodcut model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :varieties
-  has_many :photos, through: :variety
+  has_many :photos, through: :varieties
 
   validates_presence_of :product_code
   validates_uniqueness_of :product_code
