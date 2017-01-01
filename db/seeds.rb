166e1d9fdfd0f3e93859269a18b814443be9a91e
A-Boudi <abr.boudi@gmail.com>
2017-01-01 16:20:45 +0100

    Creating a larger seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,15 @@ varieties = Variety.create([{ name: 'Variety1', variety_code: 'Variety1',  produ
 places = Place.create([{ name: 'place1', place_code: 1 },
                        { name: 'place2', place_code: 2 }])
 
-photos = Photo.create([{ path: '/images/01.jpg', thumbnail_path: '/thumbnails/01.jpg', user: User.first, variety: Variety.first, place: Place.first, published: true  },
-                       { path: '/images/02.jpg', thumbnail_path: '/thumbnails/02.jpg', user: User.last,  variety: Variety.last,  place: Place.last,  published: false },
-                       { path: '/images/03.jpg', thumbnail_path: '/thumbnails/03.jpg', user: User.last,  variety: Variety.first, place: Place.last,  published: true  }])
+photos = Photo.create([{ path: '/images/01.jpg', thumbnail_path: '/thumbnails/01.jpg', user: User.first, variety: Variety.first, place: Place.last , published: true  },
+                       { path: '/images/02.jpg', thumbnail_path: '/thumbnails/02.jpg', user: User.last , variety: Variety.first, place: Place.last , published: false },
+                       { path: '/images/03.jpg', thumbnail_path: '/thumbnails/03.jpg', user: User.first, variety: Variety.last , place: Place.last , published: true  },
+                       { path: '/images/04.jpg', thumbnail_path: '/thumbnails/04.jpg', user: User.last , variety: Variety.last , place: Place.first, published: false },
+                       { path: '/images/05.jpg', thumbnail_path: '/thumbnails/05.jpg', user: User.first, variety: Variety.first, place: Place.first, published: true  },
+                       { path: '/images/06.jpg', thumbnail_path: '/thumbnails/06.jpg', user: User.last , variety: Variety.first, place: Place.first, published: true  },
+                       { path: '/images/07.jpg', thumbnail_path: '/thumbnails/07.jpg', user: User.first, variety: Variety.last , place: Place.last , published: true  },
+                       { path: '/images/08.jpg', thumbnail_path: '/thumbnails/08.jpg', user: User.last , variety: Variety.last , place: Place.last , published: true  },
+                       { path: '/images/09.jpg', thumbnail_path: '/thumbnails/09.jpg', user: User.first, variety: Variety.first, place: Place.last , published: true  },
+                       { path: '/images/10.jpg', thumbnail_path: '/thumbnails/10.jpg', user: User.last , variety: Variety.first, place: Place.first, published: true  },
+                       { path: '/images/11.jpg', thumbnail_path: '/thumbnails/11.jpg', user: User.first, variety: Variety.last , place: Place.first, published: false },
+                       { path: '/images/12.jpg', thumbnail_path: '/thumbnails/12.jpg', user: User.last , variety: Variety.last , place: Place.first, published: true  }])
