88f43e79f119c2b334f5f6ff860117c7dc4684e9
A-Boudi <abr.boudi@gmail.com>
2017-05-07 19:29:59 +0100

    Move helper methodes from the photos controller to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   private
-
     def current_user
       if user_signed_in?
         warden.user
@@ -27,4 +26,32 @@ class ApplicationController < ActionController::Base
     def warden
       env['warden']
     end
+
+    def set_photo_params
+      if !@photo.blank?
+        @product = @photo.product
+        @product_id = @product.id
+        @varieties = @product.varieties.pluck('name, id')
+        @variety_id = @photo.variety.id
+        @place_code = @photo.place.place_code
+        return
+      end
+      if !params[:product].blank?
+        @product = Product.find_by_id(params[:product])
+        @product_id = @product.id unless @product.blank?
+        @varieties = @product.varieties.pluck('name, id') unless @product.blank?
+      end
+      if !params[:photo].blank?
+        @variety_id = params[:photo][:variety_id]
+        @place_code = params[:photo][:place_id]
+      end
+    end
+
+    def get_lists_info
+      #todo: use product_code, variety_code, and place_code to hide the ID from the user
+      @products = Product.all.pluck('name, id')
+      @varieties = Variety.all.pluck('name, id')
+      @places = Place.all.pluck('name, id')
+    end
+
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :get_list_info,  except: [:index, :show]
+  before_action :get_lists_info,  except: [:index, :show]
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
   before_action :set_photo_params, except:  [:index, :show]
 
@@ -70,33 +70,6 @@ class PhotosController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
       @photo = Photo.find(params[:id])
-    end
-
-    def set_photo_params
-      if !@photo.blank?
-        @product = @photo.product
-        @product_id = @product.id
-        @varieties = @product.varieties.pluck('name, id')
-        @variety_id = @photo.variety.id
-        @place_code = @photo.place.place_code
-        return
-      end
-      if !params[:product].blank?
-        @product = Product.find_by_id(params[:product])
-        @product_id = @product.id unless @product.blank?
-        @varieties = @product.varieties.pluck('name, id') unless @product.blank?
-      end
-      if !params[:photo].blank?
-        @variety_id = params[:photo][:variety_id]
-        @place_code = params[:photo][:place_id]
-      end
-    end
-
-    def get_list_info
-      #todo: use product_code, variety_code, and place_code to hide the ID from the user
-      @products = Product.all.pluck('name, id')
-      @varieties = Variety.all.pluck('name, id')
-      @places = Place.all.pluck('name, id')
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
