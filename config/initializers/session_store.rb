70e0f8ea93247c9bbe57e020e9ae0929ca9c4685
A-Boudi <abr.boudi@gmail.com>
2018-10-13 10:43:07 +0100

    Upgraded to 5.0.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_galleryShare_session'
+Rails.application.config.session_store :cookie_store, key: '_gallery_share_session'
