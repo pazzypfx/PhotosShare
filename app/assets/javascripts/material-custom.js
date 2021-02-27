2ee83d9cfcbb612b569b86ae6980c88c39f20915
A-Boudi <abr.boudi@gmail.com>
2021-02-27 21:14:36 +0100

    Migrates form, edit, new and show pages to Material

--- app/assets/javascripts/material-custom.js
+++ app/assets/javascripts/material-custom.js
@@ -4,3 +4,8 @@ $(document).ready(function() {
         searching: false,
     });
 });
+
+$('.form-file-multiple .inputFileVisible, .form-file-multiple .input-group-btn').click(function() {
+    $(this).parent().parent().find('.inputFileHidden').trigger('click');
+    $(this).parent().parent().addClass('is-focused');
+});
